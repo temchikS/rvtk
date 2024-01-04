a8d83675511e9db19e7e09bb940c4fc2e66c56ea
temchikS <kerkyakomponaya@gmail.com>
2024-01-04 22:51:22 +0600

    hbj

--- src/components/main/prof/profession.jsx
+++
@@ -1,9 +0,0 @@
-import '../../main/main.css'
-export default function ProfCard({description,image_url}){
-    return(
-        <a href='#' className='card'>
-            <img src={image_url} alt="img" className='card-img'/>
-            <p>{description}</p>
-        </a>
-    );
-}
